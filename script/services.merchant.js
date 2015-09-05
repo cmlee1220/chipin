79f7f0e3d11b433e8b12372f656dee36d8d7ee3e
cmlee1220 <cmlee1220@gmail.com>
2015-09-04 22:59:53 -0700

    Update services.merchant.js

--- script/services.merchant.js
+++ script/services.merchant.js
@@ -1,5 +1,35 @@
 sabio.services.merchant = {};
 
+
+sabio.services.merchant.create = function (formData, onSuccess, onError) {
+	var url = "/api/merchant";//pulled RoutePrefix and Route from API controller, uset in REST client
+	var settings = {
+		cache: false
+		, contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+		, data: formData
+		, dataType: "json"
+		, success: onSuccess
+		, error: onError
+		, type: "POST"
+	};
+	$.ajax(url, settings);
+
+};
+
+sabio.services.merchant.update = function (formData, merchantId, onSuccess, onError) {
+	var url = "/api/merchant/" + merchantId;//pulled RoutePrefix and Route from API controller, used in REST client
+	var settings = {
+		cache: false
+        , contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+        , data: formData
+        , dataType: "json"
+        , success: onSuccess
+		, error: onError
+        , type: "PUT"
+	};
+	$.ajax(url, settings);
+};
+
 sabio.services.merchant.adminSelect = function (onSuccess, onError) {
     var url = "/api/admin/merchant"
     var settings = {
